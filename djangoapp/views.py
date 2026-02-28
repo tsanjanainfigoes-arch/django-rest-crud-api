2bfdd3a91c03efd85d9ca6b1a3d636cdaa346233
tsanjanainfigoes-arch <tsanjanainfigoes@gmail.com>
2026-02-28 18:02:06 +0530

    added patch condition in views.py

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -20,8 +20,8 @@ def index(request):
     elif request.method=="POST":
         return Response("This is POST method")
     
-    elif request.method=="PUT":
-        return Response("This is PUT method")
+    elif request.method=="PATCH":
+        return Response("This is PATCH method")
 
 # python to json convert using serializer
 @api_view(['POST','GET','PUT','PATCH','DELETE'])
